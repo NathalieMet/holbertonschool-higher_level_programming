a62e300124cccdf1c25683f9e5c1b467bd515cd4
nathalie <nathalie.metreau@hotmail.fr>
2023-09-27 12:50:33 +0200

    add print_combo function

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+for int in range(0, 10):
+    for digit in range(1, 10):
+        if int < 8:
+            if int != digit and int < digit:
+                print("{}{}".format(int, digit), end=", ")
+        else:
+            if int != digit and int < digit:
+                print("{}{}".format(int, digit))
