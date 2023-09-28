bcf29e62cde73e12b869d27531dbd2d5483971e2
nathalie <nathalie.metreau@hotmail.fr>
2023-09-28 12:21:03 +0200

    try to correct function hidden_discovery

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
-
-string = dir(hidden_4)
+    string = dir(hidden_4)
 filtered = sorted([name for name in string if not name.startwith("__")])
 for name in filtered:
     print(name)
