bdf2329a41168a5f085fdcf653a4a413ba6850f4
nathalie <nathalie.metreau@hotmail.fr>
2023-09-28 12:24:06 +0200

    correct the startswith in hidden_discovery function

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 import hidden_4
 if __name__ == "__main__":
     string = dir(hidden_4)
-filtered = sorted([name for name in string if not name.startwith("__")])
+filtered = sorted([name for name in string if not name.startswith("__")])
 for name in filtered:
     print(name)
