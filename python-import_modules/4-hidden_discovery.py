aaee554a355ca77a660494afc51579e219cf9eec
nathalie <nathalie.metreau@hotmail.fr>
2023-09-28 12:17:30 +0200

    correct pycode

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     import hidden_4
 
 string = dir(hidden_4)
-filtered = sorted([name for name in string if not name.startwith ("__")])
+filtered = sorted([name for name in string if not name.startwith("__")])
 for name in filtered:
-	print(name)
+    print(name)
