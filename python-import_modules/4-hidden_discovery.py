f853101e3b6f1f9072a7b985375db839fb03bd63
nathalie <nathalie.metreau@hotmail.fr>
2023-09-28 12:15:22 +0200

    add hidden_discovery function

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+
+string = dir(hidden_4)
+filtered = sorted([name for name in string if not name.startwith ("__")])
+for name in filtered:
+	print(name)
