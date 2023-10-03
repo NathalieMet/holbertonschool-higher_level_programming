b7c83c9e07f0c4f6fe5421f3870a4dd2035e83d3
nathalie <nathalie.metreau@hotmail.fr>
2023-10-03 10:25:46 +0200

    correct function print_matrix_integer

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        line = ' '.join(str(matrix[i][j]) for j in range(len(matrix[i])))
-        print(line)
+        for j in range(len(matrix[i])):
+            print("{}".format(matrix[i][j]), end='')
+            if j < len(matrix[0])-1:
+                print(" ", end='')
+        print()
