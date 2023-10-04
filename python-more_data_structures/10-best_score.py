48619770391e8e2a6dbdae4cbef6583ddcb095f7
nathalie <nathalie.metreau@hotmail.fr>
2023-10-04 15:20:29 +0200

    correct else statement in best_score function

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,4 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return (a_dictionary)
-    return (max(a_dictionary, key=a_dictionary.get))
+    else:
+        return (max(a_dictionary, key=a_dictionary.get))
