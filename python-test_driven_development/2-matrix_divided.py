425405cfbb5e5820af312c4af2a8888ac0a96f34
nathalie <nathalie.metreau@hotmail.fr>
2023-10-10 04:03:00 +0200

    correct pycodestyle in 2-matric_divided function

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -12,12 +12,15 @@ def matrix_divided(matrix, div):
     """
     if not isinstance(matrix, list) or \
         not all(isinstance(row, list) for row in matrix) or \
-        not all(isinstance(element, (int, float)) for row in matrix for element in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        not all(isinstance(element, (int, float))
+                for row in matrix
+                for element in row):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise ValueError("Each row of the matrix must have the same size")
     if not isinstance(div, (float, int)):
-        raise TypeError ("div must be a number")
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
