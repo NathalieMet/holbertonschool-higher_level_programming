72e862abe78203e245bd893474d135362b8801cb
nathalie <nathalie.metreau@hotmail.fr>
2023-10-18 22:56:21 +0200

    correct 9-rectangle function

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -9,16 +9,16 @@ class Rectangle(BaseGeometry):
     """class Rectangle"""
     def __init__(self, width, height):
         super().__init__()
-        if self.integer_validator("width", width):
-            self.__width = width
-        if self.integer_validator("height", height):
-            self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def area(self):
         return self.__width * self.__height
 
-    def print(self):
-        print ("[Rectangle] {}/{}".format(self.__width, self.__height))
-
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def print(self):
+        print("[Rectangle] {}/{}".format(self.__width, self.__height))
