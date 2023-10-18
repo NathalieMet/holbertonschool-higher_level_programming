a233dbbec8086b7b9fa44583f966454b35f23302
nathalie <nathalie.metreau@hotmail.fr>
2023-10-18 20:17:47 +0200

    correct pydecodestyle on t8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,11 +4,12 @@
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """class Rectangle"""
     def __init__(self, width, height):
         super().__init__()
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        if self.integer_validator("width", width):
+            self.__width = width
+        if self.integer_validator("height", height):
+            self.__height = height
