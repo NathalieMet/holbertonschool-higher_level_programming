48cab8a74fc3be8a364e4e21ac17bea8b8c38a0a
nathalie <nathalie.metreau@hotmail.fr>
2023-10-19 13:31:46 +0200

    add wri

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that reads a text file (UTF8) and prints it to stdout"""
+
+
+def write_file(filename="", text=""):
+    """function that reads a text file (UTF8) and prints it to stdout:"""
+    with open(filename, 'w') as f:
+        f.write(text)
+    return len(text)
