bdca2f59a18b566852c2a0de085b72c9a729c188
nathalie <nathalie.metreau@hotmail.fr>
2023-10-20 13:16:38 +0200

    correct 7-add_item f

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,8 +9,9 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 
-def load_add_save():
-    """function that creates an Object from a “JSON file”"""
+"""function that creates an Object from a “JSON file”"""
+
+if __name__ == "__main__":
     try:
         var = load_from_json_file("add_item.json")
     except FileNotFoundError:
