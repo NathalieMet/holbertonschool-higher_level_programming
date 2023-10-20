1cbdb5a393e11c85078524281da0ee9c14cf2e77
nathalie <nathalie.metreau@hotmail.fr>
2023-10-20 11:52:20 +0200

    add save_to_json_file function

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""function that returns an object (Python data structure) represented
-by a JSON string"""
+"""function that writes an Object to a text file, using a JSON
+representation:"""
 
 import json
 
 
-def from_json_string(my_str):
-    """function that returns an object (Python data structure) represented
-    by a JSON string:"""
-    return json.loads(my_str)
+def save_to_json_file(my_obj, filename):
+    """function that writes an Object to a text file, using a JSON
+    representation:"""
+    with open(filename, 'w') as f:
+        json_obj = json.dumps(my_obj)
+        f.write(json_obj)
