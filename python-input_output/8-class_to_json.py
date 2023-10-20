fe0361681f8aa44c742e2aaa971ceb252466e38c
nathalie <nathalie.metreau@hotmail.fr>
2023-10-20 13:37:16 +0200

    correct function 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,8 +3,6 @@
  (list, dictionary, string, integer and boolean) for JSON serialization of
    an object"""
 
-import json
-
 
 def class_to_json(obj):
     """function that returns the dictionary description with simple data
