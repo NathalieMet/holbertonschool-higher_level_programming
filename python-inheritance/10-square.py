c5a1297858a52dc275afa8ff97dd56a2f651e03a
nathalie <nathalie.metreau@hotmail.fr>
2023-10-20 11:09:33 +0200

    correct function 10 and 11 remove super line

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -9,7 +9,6 @@ Rectangle = __import__("9-rectangle").Rectangle
 class Square (Rectangle):
     """class Square"""
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
 
