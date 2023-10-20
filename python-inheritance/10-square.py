1843a782cd86c5d3f2383422b2f2608a82d2ad77
nathalie <nathalie.metreau@hotmail.fr>
2023-10-20 10:40:30 +0200

    try correct t10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,15 +2,16 @@
 """class Square"""
 
 
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square (Rectangle):
     """class Square"""
     def __init__(self, size):
-        self.__size = size
         super().__init__(size, size)
         self.integer_validator("size", size)
+        self.__size = size
 
     def area(self):
         return self.__size * self.__size
