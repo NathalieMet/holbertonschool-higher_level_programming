abdb75087b276300e4832ce3dd0bd24c0b6aea9b
nathalie <nathalie.metreau@hotmail.fr>
2023-10-24 15:16:31 +0200

    add t 19 function base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -50,3 +50,15 @@ class Base:
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances."""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                json_str = file.read()
+                list_dicts = cls.from_json_string(json_str)
+                return [cls.create(**d) for d in list_dicts]
+        except FileNotFoundError:
+            return[]
