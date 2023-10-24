56f6bd4d10cca269bc146716541b5e0ae865b708
nathalie <nathalie.metreau@hotmail.fr>
2023-10-24 11:28:53 +0200

    add t15 base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """class Base"""
 
+import json
+
 
 class Base:
     """class Base"""
@@ -12,3 +14,10 @@ class Base:
         else:
             self.__class__.__nb_objects += 1
             self.id = self.__class__.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
