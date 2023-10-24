3eb7d02f79a5e79c846917b843f90c39108c2ae7
nathalie <nathalie.metreau@hotmail.fr>
2023-10-24 10:52:26 +0200

    add 11 function square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,8 +9,23 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """def str"""
         return ("[Square] ({}) {}/{} - {}"
                 .format(self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
