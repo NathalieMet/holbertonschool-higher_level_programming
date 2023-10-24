9fed018789f5bf8b8bdee3e2afd5c7ec671268e9
nathalie <nathalie.metreau@hotmail.fr>
2023-10-24 13:21:09 +0200

    add function 17 base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,3 +33,10 @@ class Base:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 json_obj = cls.to_json_string(list_dicts)
                 f.write(json_obj)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None:
+            return ("[]")
+        else:
+            return json.loads(json_string)
