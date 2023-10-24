dac976a03ecd8ea7dbb075b8d6dbfa0e5573484a
nathalie <nathalie.metreau@hotmail.fr>
2023-10-24 13:22:41 +0200

    try correction on t17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -37,6 +37,6 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         if json_string is None:
-            return ("[]")
+            return []
         else:
             return json.loads(json_string)
