64abe0ad85090d8036ca21494a6843fa848b72a3
nathalie <nathalie.metreau@hotmail.fr>
2023-11-20 14:28:25 +0100

    correct t4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
 -- Write a script that lists all the tables of a database in your MySQL server. --
 Create TABLE IF NOT EXISTS first_table (
-	id:INT
-	name:VARCHAR(256)
-)
-;
+	id INT,
+	name VARCHAR(256)
+);
