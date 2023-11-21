07c8c4f5f05ce678cd0944be8b9f0b62e00a6bc4
nathalie <nathalie.metreau@hotmail.fr>
2023-11-21 13:50:17 +0100

    correct t8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,6 +3,6 @@
 SET @california_id = (SELECT id FROM states WHERE name = 'California');
 
 -- List all cities of California
-SELECT * FROM cities
+SELECT id, name FROM cities
 WHERE state_id = @california_id
 ORDER BY id ASC;
