87938360fc0a36be21d9c0884d4021b1b43719b2
nathalie <nathalie.metreau@hotmail.fr>
2023-11-27 15:24:05 +0100

    correct t7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Write a script that lists all State objects from the database hbtn_0e_6_usa"""
+"""Write a script that lists all State objects
+ from the database hbtn_0e_6_usa"""
 
 from sys import argv
 from model_state import Base, State
@@ -8,10 +9,11 @@ from sqlalchemy import create_engine
 
 if __name__ == "__main__":
     """The code will not be executed when imported"""
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                       .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 Base.metadata.create_all(engine)
 
 session = Session(engine)
-for state in session.query(State).order_by(State.id).all(): # HERE: no SQL query, only objects!
+for state in session.query(State).order_by(State.id).all():
     print("{}: {}".format(state.id, state.name))
 session.close()
