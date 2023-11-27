6450dbc4fd34b8325676ed90d550a2eeeb072d0a
nathalie <nathalie.metreau@hotmail.fr>
2023-11-27 10:22:47 +0100

    add t1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+
+def list_states():
+    """lists all states from the database hbtn_0e_0_usa"""
+
+    username, password, database = argv[1], argv[2], argv[3]
+    # Établir une connexion à la base de données
+    connexion = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database)
+    # Créer un objet curseur pour exécuter des requêtes SQL
+    curseur = connexion.cursor()
+    # Exécuter une requête SQL
+    curseur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    # Récupérer les résultats
+    query_rows = curseur.fetchall()
+    for row in query_rows:
+        print(row)
+    # Fermer le curseur et la connexion
+    curseur.close()
+    connexion.close()
+
+
+if __name__ == "__main__":
+    """The code will not be executed when imported"""
+
+    list_states()
