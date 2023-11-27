f707df0d3b19efd9dc7266e5115372cbcd12c0d4
nathalie <nathalie.metreau@hotmail.fr>
2023-11-27 15:33:38 +0100

    correct t7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,11 +9,12 @@ from sqlalchemy import create_engine
 
 if __name__ == "__main__":
     """The code will not be executed when imported"""
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-session = Session(engine)
-for state in session.query(State).order_by(State.id).all():
-    print("{}: {}".format(state.id, state.name))
-session.close()
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
